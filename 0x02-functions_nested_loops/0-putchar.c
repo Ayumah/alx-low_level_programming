48ff15dfb864dbc9c496d28a092c393202d0b871
Ayumah <angieayuma@gmail.com>
2022-09-14 10:10:14 +0000

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * 
+ *
+ * Return: On success 0.
+ *
+ */
+int _putchar(char c)
+{
+	putchar("_");
+	putchar("p");
+	putchar("u");
+	putchar("t");
+	putchar("c");
+	putchar("h");
+	putchar("a");
+	putchar("r");
+	putchar("\n");
+
+	retun (0);
+}
