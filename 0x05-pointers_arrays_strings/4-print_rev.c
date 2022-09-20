c15961364c3f0d4d4e597c46e8bfab27f7de3b3f
Ayumah <angieayuma@gmail.com>
2022-09-20 18:19:07 +0000

    Correction of 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,21 +2,21 @@
 
 /**
  * print_rev - prints a string in reverse
- * @s - string to be printed
+ * @s: string to be printed
  *
  */
 void print_rev(char *s)
 {
 	int i;
-	
-	int len;
 
-	for ( i = 0; s[i] != '\0'; i++)
+	int len = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		len++;
 	}
 
-	for (i = len - 1; i >= 0; i--)
+		for (i = len - 1; i >= 0; i--)
 		{
 			_putchar(s[i]);
 		}
