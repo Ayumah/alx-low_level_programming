c31d9987bf43275843be59c35b8c2964d062e060
Ayumah <angieayuma@gmail.com>
2022-11-08 11:49:52 +0000

    Get node at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -2,7 +2,7 @@
 
 /**
  * get_nodeint_at_index - returns a given node of a listint_t linked list.
- * 
+ *
  * @head: A pointer to the head of the listint_t list.
  * @index: The index of the node starting at 0.
  *
