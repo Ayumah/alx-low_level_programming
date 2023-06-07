5a998083220d2e90dca5efd579e763f3cb0ecbe6
Ayumah <angieayuma@gmail.com>
2023-06-07 16:24:50 +0000

    Update: 0-puts_recursion.c

--- 0-puts_recursion.c
+++ 0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _puts_recursion - Recursive finction that prints a string, followed by a new line.
- * @s: The string to be printed.
+ * @s: The string to be printed
  */
 void _puts_recursion(char *s)
 {
