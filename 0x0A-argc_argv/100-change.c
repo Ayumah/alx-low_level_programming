88c0d72d124736ac3e72884b5015871a0a4bc2d0
Ayumah <angieayuma@gmail.com>
2023-06-09 18:17:14 +0000

    Add file: 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,37 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int calculateMinCoins(int cents) {
+int calculateMinCoins(int cents)
+{
 	int coins = 0;
 	int denominations[] = {25, 10, 5, 2, 1};
 	int numDenominations = sizeof(denominations) / sizeof(denominations[0]);
 	int i;
 
-	if (cents < 0) {
-	return 0;
+	if (cents < 0)
+	{
+	return (0);
 	}
 
-	for (i = 0; i < numDenominations; i++) {
+	for (i = 0; i < numDenominations; i++)
+	{
 	coins += cents / denominations[i];
 	cents %= denominations[i];
 	}
 
-	return coins;
+	return (coins);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	int cents;
 	int minCoins;
 
-	if (argc != 2) {
+	if (argc != 2)
+	{
 	printf("Error\n");
-	return 1;
+	return (1);
 	}
 
 	cents = atoi(argv[1]);
 	minCoins = calculateMinCoins(cents);
 
 	printf("%d\n", minCoins);
-	return 0;
+	return (0);
 }
 
